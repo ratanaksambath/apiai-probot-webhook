5ceaac4a40f1c2462683859bc6be14efd5cd4bae
ratanaksambath <ratanaksambath@gmail.com>
2016-09-22 13:36:39 -0700

    bring back yahoo weather

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = postSheetsu(req)
+    res = processRequest(req)
     res = json.dumps(res, indent=4)
     print(res)
     r = make_response(res)
