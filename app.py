71d859aa0de42787a37af637579e7b4bab26f5e2
ratanaksambath <ratanaksambath@gmail.com>
2016-09-22 13:07:59 -0700

    typo

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def postSheetsu(req):
     project_manager_name = req.get("result").get("result").get("parameters").get("project_manager_name")
     values = {'created_at': created_at, 'project_title': project_title,'project_manager_name':project_manager_name}
     data = urllib.urlencode(values)
-    reqest = urllib2.Request(url,data)
+    request = urllib2.Request(url,data)
     response = urllib2.urlopen(request)
     status_code = res.getcode()
     return {
